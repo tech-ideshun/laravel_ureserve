6c48649de442aebb3c9db75c39250aa945c01b28
tech-ideshun <tech.ideshun.717@gmail.com>
2022-08-27 00:44:55 +0900

    イベント計算

--- app/Http/Livewire/Calendar.php
+++ app/Http/Livewire/Calendar.php
@@ -13,6 +13,8 @@ class Calendar extends Component
     public $currentDate;
     public $currentWeek;
     public $day;
+    public $checkDay;
+    public $dayOfWeek;
     public $sevenDaysLater;
     public $events;
 
@@ -23,12 +25,17 @@ class Calendar extends Component
         $this->currentWeek = [];
 
         $this->events = EventService::getWeekEvents($this->currentDate->format('Y-m-d'), $this->sevenDaysLater->format('Y-m-d'));
-
+        // dd($this->events);
         for($i = 0; $i < 7; $i++){
             $this->day = CarbonImmutable::today()->addDays($i)->format('m月d日');
-            array_push($this->currentWeek, $this->day);
+            $this->checkDay = CarbonImmutable::today()->addDays($i)->format('Y-m-d');
+            $this->dayOfWeek = CarbonImmutable::today()->addDays($i)->dayName;
+            array_push($this->currentWeek, [
+                'day' => $this->day,
+                'checkDay' => $this->checkDay,
+                'dayOfWeek' => $this->dayOfWeek
+            ]);
         }
-
         // dd($this->currentWeek);
     }
 
@@ -42,7 +49,13 @@ class Calendar extends Component
 
         for($i = 0; $i < 7; $i++){
             $this->day = CarbonImmutable::parse($this->currentDate)->addDays($i)->format('m月d日');
-            array_push($this->currentWeek, $this->day);
+            $this->checkDay = CarbonImmutable::parse($this->currentDate)->addDays($i)->format('Y-m-d');
+            $this->dayOfWeek = CarbonImmutable::parse($this->currentDate)->addDays($i)->dayName;
+            array_push($this->currentWeek, [
+                'day' => $this->day,
+                'checkDay' => $this->checkDay,
+                'dayOfWeek' => $this->dayOfWeek,
+            ]);
         }
     }
 
